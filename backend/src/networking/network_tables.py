9c5877d0e94f90a96329e6280d86ea74a35ec55a
EitanCo <intheocean.email@gmail.com>
2024-12-29 17:44:35 +0200

    added skeleton for network methods

---
+++ backend/src/networking/network_tables.py
@@ -0,0 +1,16 @@
+"""
+This function takes input in the "data_format"; see utils/data_formats.py
+This function updates the NetworkTables with this input.
+"""
+def update_robot_data(robot_data: dict):
+    # TODO: @NoamHSU
+    raise NotImplementedError("This function has not been implemented yet.")
+
+
+"""
+This function gets a camera list; see capture/camera.py
+It should update the camera streams on the network (camera.frame is already constantly updating in the back)
+"""
+def update_streams(cameras: list):
+    # TODO: @NoamHSU
+    raise NotImplementedError("This function has not been implemented yet.")
